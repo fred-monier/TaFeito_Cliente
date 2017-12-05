a4b064beec0c170d4a980ef0fc39e912001b6f6f
leomaarques <leonardogm_16@hotmail.com>
2017-12-05 16:30:50 -0300

    Telas Ok

--- app/src/main/java/br/pe/recife/tafeito/gui/ClientePrincipalActivity.java
+++ app/src/main/java/br/pe/recife/tafeito/gui/ClientePrincipalActivity.java
@@ -1,7 +1,9 @@
 package br.pe.recife.tafeito.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import br.pe.recife.tafeito.R;
 
@@ -11,5 +13,25 @@ public class ClientePrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_principal);
+    }
+
+    public void irConfig(View view) {
+        Intent intent = new Intent(this, SettingsClientActivity.class);
+        startActivity(intent);
+    }
+
+    public void irLogin(View view) {
+        Intent intent = new Intent(this, ClienteLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void irSchedule(View view) {
+        Intent intent = new Intent(this, ScheduleClientActivity.class);
+        startActivity(intent);
+    }
+
+    public void irOffer(View view) {
+        Intent intent = new Intent(this, OfferClientActivity.class);
+        startActivity(intent);
     }
 }
